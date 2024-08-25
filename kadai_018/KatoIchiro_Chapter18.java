9f6c62fdee00aaaf37c9a7b6df689bd5ac8c11b9
chisato777 <takaharachisato@gmail.com>
2024-08-25 12:46:52 +0900

    最初のコミット

---
+++ kadai_018/KatoIchiro_Chapter18.java
@@ -0,0 +1,15 @@
+package kadai_018;
+
+public class KatoIchiro_Chapter18 extends Kato_Chapter18 {
+	
+	public String givenName = "一郎";
+	
+	public void setGivenName(String givenName) {
+		System.out.println(givenName);
+	}
+	
+	public void eachIntroduce() {
+		System.out.println("好きな食べ物はリンゴです");
+	}
+
+}
